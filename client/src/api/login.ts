c5c7ac85f5b22451320fdb8efecb0a9f5ef2260e
abhinas regmi <aviregmi2018@gmail.com>
2024-02-27 21:19:14 +0545

    Fix login API method and add withCredentials option

--- client/src/api/login.ts
+++ client/src/api/login.ts
@@ -4,15 +4,18 @@ import { serverBaseUrl } from '@/lib/data';
 export async function login(){
     return axios({
         url: serverBaseUrl + '/auth/login',
-        method: 'GET'
+        method: 'get'
     })
 }
 
 export async function getUserData(token: string){
     return axios({
         url: serverBaseUrl + '/user/info',
+        method: 'get',
+        withCredentials: false,
         headers: {
             Authorization: `Bearer ${token}`,
+            
         }
     })
 }
