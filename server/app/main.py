6d7bb5260bbb05998add2ca40f4cbd4a55df90cb
abhinas regmi <aviregmi2018@gmail.com>
2024-02-27 21:10:07 +0545

    Update CORS origins in main.py

--- server/app/main.py
+++ server/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=['http://localhost:5173', 'https://blog.abhinasregmi.com.np'],
     allow_methods=['*'],
     allow_headers=['*'],
     allow_credentials=True
