5a6e42e07edcefdb77cb8d63db14c1d6c96f7478
abhinas regmi <aviregmi2018@gmail.com>
2024-02-27 20:54:14 +0545

    Update server and client base URLs

--- client/src/lib/data.ts
+++ client/src/lib/data.ts
@@ -1,2 +1,2 @@
-export const serverBaseUrl = 'http://localhost:8000';
-export const clientBaseUrl = 'http://localhost:5173';
+export const serverBaseUrl = 'https://blog-ufig.onrender.com';
+export const clientBaseUrl = 'https://blog.abhinasregmi.com.np';
